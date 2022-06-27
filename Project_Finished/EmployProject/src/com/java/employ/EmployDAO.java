385c39fd9c466df8eaf50b4b3d0592e8542cd22d
prasannakasyap <puiangtraining@gmail.com>
2022-06-27 12:38:21 +0530

    code

---
+++ Project_Finished/EmployProject/src/com/java/employ/EmployDAO.java
@@ -0,0 +1,58 @@
+package com.java.employ;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployDAO {
+
+	static List<Employ> employList;
+	
+	static {
+		employList = new ArrayList<Employ>();
+	}
+	
+	public String updateEmployDao(Employ employNew) {
+		Employ employFound = searchEmployDao(employNew.getEmpno());
+		if (employFound!=null) {
+			for (Employ employ : employList) {
+				if (employ.getEmpno()==employNew.getEmpno()) {
+					employ.setName(employNew.getName());
+					employ.setGender(employNew.getGender());
+					employ.setDept(employNew.getDept());
+					employ.setDesig(employNew.getDesig());
+					employ.setBasic(employNew.getBasic());
+					break;
+				}
+			}
+			return "Employ Record Updated..."; 
+		}
+		return "Employ Record Not Found...";
+	}
+	public String deleteEmployDao(int empno) {
+		Employ employFound = searchEmployDao(empno);
+		if (employFound!=null) {
+			employList.remove(employFound);
+			return "Employ Record Deleted...";
+		}
+		return "Employ Record Not Found...";
+	}
+	
+	public Employ searchEmployDao(int empno) {
+		Employ employFound = null;
+		for (Employ employ : employList) {
+			if (employ.getEmpno()==empno) {
+				employFound = employ;
+			}
+		}
+		return employFound;
+	}
+	
+	public String addEmployDao(Employ employ) {
+		employList.add(employ);
+		return "Employ record Inserted...";
+	}
+	
+	public List<Employ> showEmployDao() {
+		return employList;
+	}
+}
